d58a98b7f7b35cc9c7a51014f5d98b54da1cde0b
shlomi <shlomi10@gmail.com>
2019-04-19 15:27:49 +0300

    first upload

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,56 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.*;
+
+import pages.AddEmpPage;
+import pages.EmployeeList;
+import pages.LeaveListPage;
+import pages.LoginPage;
+import utilities.ExtentManager;
+
+
+public class BaseTest extends ExtentManager {
+
+	WebDriver driver;
+	LoginPage loginPage;
+	EmployeeList pimEmpList;
+	AddEmpPage addEmpPage;
+	LeaveListPage leaveList;
+
+	@Parameters({ "browser" })
+	@BeforeTest
+	public void openBrowser(String browser) {
+
+		try {
+			if (browser.equalsIgnoreCase("fireFox")) {
+				System.setProperty("webdriver.ie.driver", ".\\drivers\\geckodriver.exe");
+				driver = new InternetExplorerDriver();
+			} else if (browser.equalsIgnoreCase("chrome")) {
+				System.setProperty("webdriver.chrome.driver" , ".\\drivers\\chromedriver.exe");
+				driver = new ChromeDriver();
+			} 
+		} catch (WebDriverException e) {
+			System.out.println(e.getMessage());
+		}
+		driver.manage().window().maximize();
+
+		//create reporting folder and report
+		init();
+
+		loginPage = new LoginPage(driver);
+		pimEmpList = new EmployeeList(driver);
+		addEmpPage = new AddEmpPage (driver);
+		leaveList = new LeaveListPage(driver);
+	}
+
+
+	@AfterTest
+	public void closeBrowser() {
+		driver.quit();
+		extent.flush(); 
+	}
+}
